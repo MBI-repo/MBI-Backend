c7e62a7977b3ab0ac42a674197f34911178d90e6
Pelumi <fasanyafemi@gmail.com>
2025-10-26 18:55:31 +0100

    waiting list

---
+++ app/Models/WaitingList.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WaitingList extends Model
+{
+    protected $guarded = [];
+    protected $table = 'waiting_lists';
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,17 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\WaitingListController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/waiting-list', [WaitingListController::class, 'store']);
+Route::get('/waiting-list', [WaitingListController::class, 'index']);
 
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
