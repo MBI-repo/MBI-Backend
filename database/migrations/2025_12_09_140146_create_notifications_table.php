4ed5865ab34f35419766a69e83aaf1c5a84f75fe
jamesanwoh <72976823+jamesanwoh@users.noreply.github.com>
2025-12-09 08:12:52 -0800

    update on notification schema

--- database/migrations/2025_12_09_140146_create_notifications_table.php
+++ database/migrations/2025_12_09_140146_create_notifications_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-       Schema::create('notification_settings', function (Blueprint $table) {
+       Schema::create('notifications', function (Blueprint $table) {
             $table->id();
             $table->uuid('user_uuid')->unique();
             $table->boolean('notify_network')->default(true);
