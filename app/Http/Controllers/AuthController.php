a800de749b5146417d3a78dcbc58f6448c8ffa54
jamesanwoh <72976823+jamesanwoh@users.noreply.github.com>
2026-03-05 12:03:30 -0800

    update on auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -136,7 +136,15 @@ class AuthController extends Controller
         }
 
         $token = $user->createToken('auth_token')->plainTextToken;
-        $user->isseller = $user->category === 'Manufacturing, Supply & Logistics';
+        //$user->isseller = $user->category === 'Manufacturing, Supply & Logistics';
+
+        $user->isseller  =
+        $user->category === 'Manufacturing, Supply & Logistics' &&
+        $user->corporate_registration_papers &&
+        $user->product_approval &&
+        $user->warranty_policy_document &&
+        $user->export_capability_statement &&
+        $user->incoterms_preference;
 
         return response()->json([
             'success' => true,
