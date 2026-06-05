1c130ef74245541f444d29b8a2ddf511f04b022b
Pelumi <fasanyafemi@gmail.com>
2026-06-05 13:23:04 +0100

    fixing cors

--- config/cors.php
+++ config/cors.php
@@ -29,6 +29,7 @@ return [
         'http://127.0.0.1:3000',
         'http://localhost:3001',
         'http://127.0.0.1:3001',
+        'http://127.0.0.1:3002',
     ],
 
     // 'allowed_origins' => ['*'],
