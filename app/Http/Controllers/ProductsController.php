10791ee85defba987ab98c5fcf32c6f617ae8520
Pelumi <fasanyafemi@gmail.com>
2026-06-16 13:04:06 +0100

    checking storage

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -108,6 +108,7 @@ class ProductsController extends Controller
                 'message' => 'Product not found',
             ], 404);
         }
+        $pel_url = 'https://testinggit.com';
         $baseUrl = 'https://api.mybridgeinternational.org/mybridge-backend-files/storage/app/public/';
         $productUrl = 'https://portal.mybridgeinternational.org/mbi-portal-files/public/';
         $donatedUrl = 'https://admin.mybridgeinternational.org/mbi-admin-files/public/';
